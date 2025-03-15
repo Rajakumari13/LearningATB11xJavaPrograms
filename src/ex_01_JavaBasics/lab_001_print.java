4ef5acf619dea7ad0d421fd5ef364ad13f55431b
Rajakumari13 <rajijeyaram13@gmail.com>
2025-03-15 08:46:09 +0530

    15.03.25- Commit & push the 1st exercise.

---
+++ src/ex_01_JavaBasics/lab_001_print.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasics;
+
+public class lab_001_print {
+    public static void main(String[] args) {
+        System.out.println("Raji Rudra's MoM");
+    }
+}
---
+++ src/ex_01_NewJavaBasics/sample_exe_print.java
@@ -0,0 +1,7 @@
+package ex_01_NewJavaBasics;
+
+public class sample_exe_print {
+    public static void main(String[] args) {
+        System.out.println("Have a nice day");
+    }
+}
