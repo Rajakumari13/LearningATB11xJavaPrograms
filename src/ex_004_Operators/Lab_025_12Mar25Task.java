ed84d243844dcba67b8a49e6b287accafc747e79
Rajakumari13 <rajijeyaram13@gmail.com>
2025-03-21 21:47:03 +0530

    21.03.25- Commit & push the exercises taken on 12.03.25

--- src/ex_004_Operators/Lab_025_12Mar25Task.java
+++ src/ex_004_Operators/Lab_025_12Mar25Task.java
@@ -4,11 +4,13 @@ public class Lab_025_12Mar25Task {
     public static void main(String[] args) {
         int x=10;
         int y=10;
-        int Z=10;
-        double x2= Math.sqrt(x);
+        int z=10;
+        System.out.println(Math.cbrt((Math.pow(x,2))+(Math.pow(y,2))-Math.abs(z)));
+       /* double x2= Math.sqrt(x);
         double y2= Math.sqrt(y);
-        double z2= Math.sqrt(Z);
+        int z2= Math.abs(z);
         System.out.println(Math.cbrt(x2+y2-z2));
+*/
 
     }
 }
